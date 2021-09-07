557ff8837426e7c89d6ef12eab3a41c03e329b81
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-09-07 23:39:16 +0300

    add response interceptor to axios

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,11 +1,15 @@
-import axios, { AxiosInstance } from 'axios';
+import axios, { AxiosInstance, AxiosResponse, AxiosError } from 'axios';
 
 const BACKEND_URL = 'https://7.react.pages.academy/wtw';
 const REQUEST_TIMEOUT = 5000;
 
 const getToken = () => localStorage.getItem('token') ?? '1';
 
-export const createApi = (): AxiosInstance => {
+const HttpCode = {
+  UNAUTHORIZED: 401,
+};
+
+export const createApi = (onUnauthorized: () => void): AxiosInstance => {
   const api = axios.create({
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
@@ -13,6 +17,20 @@ export const createApi = (): AxiosInstance => {
       'x-token': getToken(),
     },
   });
+
+  const onSuccess = (response: AxiosResponse) => response;
+
+  const onFail = (err: AxiosError) => {
+    const {response} = err;
+
+    if (response) {
+      if (response.status === HttpCode.UNAUTHORIZED) {
+        onUnauthorized();
+      }
+    }
+
+    throw err;
+  };
 
   api.interceptors.request.use((config) =>
     Object.assign({},
@@ -25,6 +43,8 @@ export const createApi = (): AxiosInstance => {
       },
     ),
   );
+
+  api.interceptors.response.use(onSuccess, onFail);
 
   return api;
 };
